00c5f1896984f7b8e64bfd05303097627ef63f0a
dec1218 <decflorhenuy.202301455@gmail.com>
2024-09-23 16:07:07 +0800

    update

---
+++ app.js
@@ -0,0 +1,22 @@
+function myFunction() {
+  let name = document.getElementById("name").value;
+  let age = document.getElementById("age").value;
+  let email = document.getElementById("email").value;
+  let hobby = document.getElementById("hobby").value;
+  console.log(name);
+  console.log(age);
+  console.log(email);
+  console.log(hobby);
+
+  const user = {
+    name: name,
+    age: age,
+    email: email,
+    hobby: hobby,
+  };
+
+  const myJSON = JSON.stringify(user);
+  console.log(user);
+  console.log(myJSON);
+  document.getElementById("jsonOutput").innerHTML = myJSON;
+}
